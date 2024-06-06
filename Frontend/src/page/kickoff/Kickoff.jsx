69351da2466f5f1ee965d733908c47436c8ca052
Hasibul Hasan <hasibulhasan442@gmail.com>
2024-06-06 09:00:44 +0000

    chore: Update background styling and npm dependencies

--- Frontend/src/page/kickoff/Kickoff.jsx
+++ Frontend/src/page/kickoff/Kickoff.jsx
@@ -1,40 +1,157 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import TextField from '@mui/material/TextField';
+import style from './kickoff.module.css'
+import { MenuItem, NativeSelect, Select } from '@mui/material';
+import axios from 'axios';
 
 function Kickoff() {
     const [numberOfPlayers, setNumberOfPlayers] = useState(0)
+    const [numberOfTeams, setNumberOfTeams] = useState(0)
+    const [data, setData] = useState([]);
+    const BaseURL = import.meta.env.VITE_API_BASE_URL;
+    const [playerName, setPlayerName] = useState([])
+    const [teamsData, setTeamsData] = useState([])
+
+    const getPlayers = async () => {
+        axios.get(`${BaseURL}/players`).then((response) => {
+            setData(response.data)
+        })
+    }
+    const getTeams = async () => {
+        axios.get(`${BaseURL}/teams`).then((response) => {
+            setData(response.data)
+        })
+    }
+
+    const handleNumberOfPlayers = (e) => {
+        const value = e.target.value;
+        if (value > 0) {
+            setNumberOfPlayers(parseInt(value));
+        } else {
+            setNumberOfPlayers(0);
+            setPlayerName([]);
+        }
+    }
+
+    const handleNumberOfTeam = (e) => {
+        const value = e.target.value;
+        if (value >= 0) {
+            setNumberOfTeams(parseInt(value));
+        }
+    }
+
+    const handlePlayerName = (e, index) => {
+        const value = e.target.value;
+        setPlayerName(prevState => {
+            const newState = [...prevState];
+            newState[index] = newState[index] || 'Default Value';
+            newState[index] = value;
+            return newState;
+        });
+    }
+
+    useEffect(() => {
+        getPlayers()
+        getTeams()
+    }, []);
+
+    console.log(teamsData)
     return (
         <>
             <div className="container">
-                <form>
-                    <div className="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                <div className="mt-2 ">
+                    <div className="row d-flex justify-content-around parentCard">
+                        <div className="col-md-4">
+                            <div className={`${style.customCard} card`}>
+                                <div className="card-body text-center">
+                                    <TextField
+                                        id="outlined-basic"
+                                        label="Number Of Player"
+                                        color="success"
+                                        variant="standard"
+                                        style={{ width: '100%' }}
+                                        value={numberOfPlayers}
+                                        onChange={handleNumberOfPlayers}
+                                        type='number'
+                                        min={0}
+                                    />
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect1">Example select</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
+
+                    <div className="row d-flex justify-content-around secondBlock">
+                        {Number.isInteger(numberOfPlayers) && numberOfPlayers >= 0 && [...Array(numberOfPlayers)].map((_, index) => (
+                            <div className="col-md-3 mt-3" key={index}>
+                                <div className={`${style.customCard} card`}>
+                                    <div className="card-body text-center">
+                                        <Select
+                                            labelId="demo-simple-select-filled-label"
+                                            id="demo-simple-select-filled"
+                                            defaultValue=""
+                                            style={{ width: '100%' }}
+                                            onChange={(e) => handlePlayerName(e, index)}
+                                        >
+                                            <MenuItem value="">
+                                                <em>None</em>
+                                            </MenuItem>
+                                            {data.map((player) => (
+                                                <MenuItem value={player.id} key={player.id}>{player.name}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect2">Example multiple select</label>
-                        <select multiple className="form-control" id="exampleFormControlSelect2">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
+
+                    {playerName.length > 0 && (
+                        <div className="row d-flex justify-content-around mt-3">
+                            <div className="col-md-4">
+                                <div className={`${style.customCard} card`}>
+                                    <div className="card-body text-center">
+                                        <TextField
+                                            id="outlined-basic"
+                                            label="Number Of Team"
+                                            color="success"
+                                            variant="standard"
+                                            style={{ width: '100%' }}
+                                            value={numberOfTeams}
+                                            onChange={handleNumberOfTeam}
+                                            type='number'
+                                            min={0}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+                    <div className="row d-flex justify-content-around secondBlock">
+                        {Number.isInteger(numberOfTeams) && numberOfTeams >= 0 && [...Array(numberOfTeams)].map((_, index) => (
+                            <div className="col-md-3 mt-3" key={index}>
+                                <div className={`${style.customCard} card`}>
+                                    <div className="card-body text-center">
+                                        <Select
+                                            labelId="demo-simple-select-filled-label"
+                                            id="demo-simple-select-filled"
+                                            defaultValue=""
+                                            style={{ width: '100%' }}
+                                            onChange={(e) => handlePlayerName(e, index)}
+                                        >
+                                            <MenuItem value="">
+                                                <em>None</em>
+                                            </MenuItem>
+                                            {teamsData.map((teams) => (
+                                                <MenuItem value={teams.id} key={teams.id}>{teams.name}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                </form>
+                </div>
             </div>
         </>
     )
