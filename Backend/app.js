a6e3d3e6d1557a75f4c05348db6f0d3376e02ea7
Rubyet Hossain <52467997+Rubyet@users.noreply.github.com>
2024-06-06 04:41:47 +0000

    updated location added global locations

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,27 @@ const app = express();
 const kickoff = require('./src/routes/kickoffApiRoutes');
 const port = 3000;
 
+const os = require('os');
+
+// Get the hostname
+const hostname = os.hostname();
+
+// Set the public/image location
+const publicImageLocation = '/public/image';
+
+// Store them in global variables
+global.hostname = hostname;
+global.publicImageLocation = publicImageLocation;
+global.logoLocation = hostname + publicImageLocation + '/logos/'
+global.playersLocation = hostname + publicImageLocation + '/players/'
+
 app.use(cors());
 
 app.get('/', (req,res) => {
     res.send("Backend working just perfect!!! Enjoy!!");
 });
+
+
 
 app.use('/api', kickoff);
 
