dde5aff414e7f300271679b488e8d393fc7d0ff3
Hasibul Hasan <hasibulhasan442@gmail.com>
2024-06-12 08:30:04 +0000

    refactor: Update Fixtures page to use Material-UI Table component
    This commit updates the Fixtures page to use the Material-UI Table component for displaying the player table. The existing HTML table has been replaced with the Table, TableContainer, TableHead, TableCell, and TableRow components from the @mui/material package. This change improves the overall styling and functionality of the player table.

--- Frontend/src/page/fixtures/Fixtures.jsx
+++ Frontend/src/page/fixtures/Fixtures.jsx
@@ -8,6 +8,7 @@ import fixtureData from "../../component/fixtureData";
 import { useParams } from "react-router-dom";
 import Loading from "../../component/loading/Loading";
 import { Flipper, Flipped } from 'react-flip-toolkit';
+import { Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 
 function Fixtures() {
   const BaseURL = import.meta.env.VITE_API_BASE_URL;
@@ -86,78 +87,23 @@ function Fixtures() {
                 <div className="col-md-4">
                   <div className={style.card}>
                     <div className={`${style.cardHeader}`}>
-                      <h5 className={style.cardHeaderText}>Point Table</h5>
+                      <h5 className={style.cardHeaderText}>Player Table</h5>
                     </div>
                     <div className="card-body">
-                      <table className="table table-sm table-responsive-lg">
-                        <thead>
-                          <tr>
-                            <th scope="col">Rank</th>
-                            <th scope="col">Team</th>
-                            <th scope="col">Played</th>
-                            <th scope="col">Won</th>
-                            <th scope="col">Drawn</th>
-                            <th scope="col">Lost</th>
-                            <th scope="col">Points</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>FC Champs</td>
-                            <td>10</td>
-                            <td>8</td>
-                            <td>1</td>
-                            <td>1</td>
-                            <td>25</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">2</th>
-                            <td>Tigers</td>
-                            <td>10</td>
-                            <td>7</td>
-                            <td>2</td>
-                            <td>1</td>
-                            <td>23</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td>FC Champs</td>
-                            <td>10</td>
-                            <td>6</td>
-                            <td>2</td>
-                            <td>2</td>
-                            <td>20</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">4</th>
-                            <td>FC Champs</td>
-                            <td>10</td>
-                            <td>5</td>
-                            <td>3</td>
-                            <td>2</td>
-                            <td>18</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">5</th>
-                            <td>FC Champs</td>
-                            <td>10</td>
-                            <td>4</td>
-                            <td>3</td>
-                            <td>3</td>
-                            <td>15</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">6</th>
-                            <td>FC Champs</td>
-                            <td>10</td>
-                            <td>3</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>12</td>
-                          </tr>
-                        </tbody>
-                      </table>
+                      <TableContainer>
+                        <Table>
+                          <TableHead>
+                            <TableRow>
+                              <TableCell>Name</TableCell>
+                              <TableCell align="right">Calories</TableCell>
+                              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                          </TableHead>
+                        </Table>
+                      </TableContainer>
+
                     </div>
                   </div>
                   <div className={style.card}>
